4e443d7ce2ad6a5a71cb82376bfd1dacae52a7c0
Arthur Shcherban <kruasain@gmail.com>
2024-01-02 15:01:52 +0200

    Made game states logic

--- Pong/main.lua
+++ Pong/main.lua
@@ -34,6 +34,9 @@ function love.load()
     font = love.graphics.newFont('font.ttf')
     love.graphics.setFont(font, 60)
 
+    gameState = 'serving'
+    playerServing = 1
+
     player1 = Paddle(10, VIRTUAL_HEIGHT / 2, 15, 65)
     player2 = Paddle(VIRTUAL_WIDTH - 30 - 10, VIRTUAL_HEIGHT / 2, 15, 65)
     ball = Ball(VIRTUAL_WIDTH / 2, VIRTUAL_HEIGHT / 2, 15, 15)
@@ -42,69 +45,92 @@ end
 function love.update(dt)
     love.window.setTitle('FPS: '..tostring(getFPS()) .. ' Ball DX: ' .. tostring(ball.dx) .. ' Ball DY: ' .. tostring(ball.dy))
  
-    -- Player 1 control logic
-    if love.keyboard.isDown('w') then
-        player1.dy = -PADDLE_SPEED
-    elseif love.keyboard.isDown('s') then
-        player1.dy = PADDLE_SPEED
-    else
-        player1.dy = 0
-    end
-    
-    -- Player 2 control logic. TODO: change AI behaviour
-    if (USE_AI) then
-        player2.y = ball.y / 2 > player2.y / 2 and player2.y + (ball.y - player2.y) or player2.y - (player2.y - ball.y)
-    else
-        if love.keyboard.isDown('up') then
-            player2.dy = -PADDLE_SPEED
-        elseif love.keyboard.isDown('down') then
-            player2.dy = PADDLE_SPEED
+    if gameState == 'running' then
+        -- Player 1 control logic
+        if love.keyboard.isDown('w') then
+            player1.dy = -PADDLE_SPEED
+        elseif love.keyboard.isDown('s') then
+            player1.dy = PADDLE_SPEED
         else
-            player2.dy = 0
+            player1.dy = 0
         end
-    end
+        
+        -- Player 2 control logic. TODO: change AI behaviour
+        if (USE_AI) then
+            player2.y = ball.y / 2 > player2.y / 2 and player2.y + (ball.y - player2.y) or player2.y - (player2.y - ball.y)
+        else
+            if love.keyboard.isDown('up') then
+                player2.dy = -PADDLE_SPEED
+            elseif love.keyboard.isDown('down') then
+                player2.dy = PADDLE_SPEED
+            else
+                player2.dy = 0
+            end
+        end
+        
+        -- Logic of the ball collision with paddles
+        if ball:collides(player1) then
+            ball.x = ball.x + 10
+            ball.dx = -ball.dx * 1.05
+            ball.dy = math.random(ball.dy - 30, ball.dy + 30)
+        elseif ball:collides(player2) then
+            ball.x = ball.x - 10
+            ball.dx = -ball.dx * 1.05
+            ball.dy = math.random(ball.dy - 30, ball.dy + 30)
+        end
     
-    -- Logic of the ball collision with paddles
-    if ball:collides(player1) then
-        ball.x = ball.x + 10
-        ball.dx = -ball.dx * 1.05
-        ball.dy = -math.random(ball.dy - 30, ball.dy + 30)
-    elseif ball:collides(player2) then
-        ball.x = ball.x - 10
-        ball.dx = -ball.dx * 1.05
-        ball.dy = -math.random(ball.dy - 30, ball.dy + 30)
+        -- Left and Right bounds logic
+        if ball.x < 0 then
+            gameState = 'serving'
+            ball:reset()
+            ball.dx = math.random(-200, -300)
+            playerServing = 2
+            player2.score = player2.score + 1
+        elseif ball.x + ball.width > VIRTUAL_WIDTH then
+            gameState = 'serving'
+            ball:reset()
+            ball.dx = math.random(200, 300)
+            playerServing = 1
+            player1.score = player1.score + 1
+        end
+    
+        -- Wall bounds logic
+        if ball.y < 0 or ball.y + ball.height > VIRTUAL_HEIGHT then
+            ball.dy = -math.random(ball.dy - 50, ball.dy + 50)
+        end
+    
+        -- Speed control
+        if ball.dx > 1000 then
+            ball.dx = ball.dx / 2
+        end
+
+        -- Score control
+        if player1.score == 10 then
+            player1.score = 0
+            gameState = 'end'
+            playerServing = 1
+        elseif player2.score == 10 then
+            player2.score = 0
+            gameState = 'end'
+            playerServing = 2
+        end
+    
+        player1:update(dt)
+        player2:update(dt)
+    
+        ball:update(dt)
+    else
+        
     end
-
-    -- Left and Right bounds logic
-    if ball.x < 0 then
-        ball:reset()
-        player2.score = player2.score + 1
-    elseif ball.x + ball.width > VIRTUAL_WIDTH then
-        ball:reset()
-        player1.score = player1.score + 1
-    end
-
-    -- Wall bounds logic
-    if ball.y < 0 or ball.y + ball.height > VIRTUAL_HEIGHT then
-        ball.dy = -math.random(ball.dy - 50, ball.dy + 50)
-    end
-
-    -- Speed control
-    if ball.dx > 1000 then
-        ball.dx = ball.dx / 2
-    end
-
-    player1:update(dt)
-    player2:update(dt)
-
-    ball:update(dt)
 end
 
 function love.keypressed(key)
     if key == 'escape' then
         love.event.quit()
-    elseif key == 'enter' or key == 'return' then
-        ball:reset()
+    elseif gameState == 'serving' or gameState == 'end' then
+        if love.keyboard.isDown('return') then
+            gameState = 'running'
+        end
     elseif key == 'c' then
         USE_AI = USE_AI == false and true or false
     end
@@ -115,42 +141,63 @@ function love.draw()
 
     love.graphics.clear(30/255, 30/255, 30/255, 255/255)
 
-    drawSeparator()
+    if gameState == 'running' or gameState == 'serving' then
 
-    player1:render()
-    player2:render()
+        drawSeparator()
 
-    ball:render()
+        player1:render()
+        player2:render()
 
-    love.graphics.printf(
-        tostring(player1.score),
-        VIRTUAL_WIDTH / 4,
-        VIRTUAL_HEIGHT / 4,
-        VIRTUAL_WIDTH,
-        'left'
-    )
+        ball:render()
 
-    love.graphics.printf(
-        tostring(player2.score),
-        VIRTUAL_WIDTH / 2 + VIRTUAL_WIDTH / 4,
-        VIRTUAL_HEIGHT / 4,
-        VIRTUAL_WIDTH,
-        'left'
-    )
-
-    if (USE_AI) then
         love.graphics.printf(
-            'AI: Enabled',
-            0,
+            tostring(player1.score),
+            VIRTUAL_WIDTH / 4,
             VIRTUAL_HEIGHT / 4,
             VIRTUAL_WIDTH,
-            'center'
+            'left'
         )
+
+        love.graphics.printf(
+            tostring(player2.score),
+            VIRTUAL_WIDTH / 2 + VIRTUAL_WIDTH / 4,
+            VIRTUAL_HEIGHT / 4,
+            VIRTUAL_WIDTH,
+            'left'
+        )
+
+        if gameState == 'running' then
+            if USE_AI == true then
+                love.graphics.printf(
+                    'AI: Enabled',
+                    0,
+                    VIRTUAL_HEIGHT / 4,
+                    VIRTUAL_WIDTH,
+                    'center'
+                )
+            else
+                love.graphics.printf(
+                    'AI: Disabled',
+                    0,
+                    VIRTUAL_HEIGHT / 4,
+                    VIRTUAL_WIDTH,
+                    'center'
+                )
+            end
+        elseif gameState == 'serving' then
+            love.graphics.printf(
+                'Serving Player ' .. tostring(playerServing),
+                0,
+                VIRTUAL_HEIGHT - VIRTUAL_HEIGHT / 5,
+                VIRTUAL_WIDTH,
+                'center'
+            )
+        end
     else
         love.graphics.printf(
-            'AI: Disabled',
+            'Player ' .. tostring(playerServing) .. ' win!\nPress Enter to Restart',
             0,
-            VIRTUAL_HEIGHT / 4,
+            VIRTUAL_HEIGHT / 2,
             VIRTUAL_WIDTH,
             'center'
         )
@@ -172,13 +219,3 @@ end
 function getFPS()
     return love.timer.getFPS()
 end
-
-
-
-
-
-
-
-
-
-
